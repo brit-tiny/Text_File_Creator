5c6f95255baa2ae0f221367d8a923e061f173dbe
brit-tiny <breakfast.at.brittinys@gmail.com>
2025-06-11 18:12:54 -0400

    update

--- file_creator.py
+++ file_creator.py
@@ -1,12 +1,12 @@
 name = input("What's your name? \n")
 color = input("What's your favorate color? \n")
 
-created = f"{name} created this file\n"
-favorite = f"Their favorite color is {color}\n"
+creator = f"{name} created this file.\n"
+favorite = f"Their favorite color is {color}.\n"
 
 with open('example.txt', 'w') as file:
-    file.write(created)
+    file.write(creator)
     file.write(favorite)
 
-print(created)
+print(creator)
 print(favorite)
